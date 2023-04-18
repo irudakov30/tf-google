939538ba0b8da80a0658f5f7c40c03f5cdc5ac5d
iru <rudakov.it@gmail.com>
2023-04-19 00:23:19 +0200

    initial tf conf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "<PROJECT_ID>"
+  region  = "<REGION>"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = "<ZONE>"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+  }
+}
